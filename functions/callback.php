8d115abb6245771904d8400fd073032818cec40e
Ndolo <imanindolo77@gmail.com>
2025-02-17 16:39:47 +0300

    mpesa integrated

--- functions/callback.php
+++
@@ -1,7 +0,0 @@
-<?php
-header("Content-Type: application/json");
-$PaystackCallbackResponse = file_get_contents('php://input');
-$logFile = "Paystackresponse.json";
-$log = fopen($logFile, "a");
-fwrite($log, $PaystackCallbackResponse);
-fclose($log);
--- utils.php
+++ utils.php
@@ -1,26 +1,33 @@
 <?php
-session_start();
 
-$errors  = array();
-$errmsg  = '';
+include "./functions/dbconnect.php";
 
-$config = array(
-    "env"              => "sandbox",
-    "BusinessShortCode"=> "174379",
-    "key"              => "32kCGgGrKXtdjJeinekBuNrhHrrAmW9le0KXIrPISq4Ag2HO", //Enter your consumer key here
-    "secret"           => "QYQR6e71VQSYYPO415RhMR352bap2SGea9ybUfSIT1KkgLckAGXXExvNShHc2A6t", //Enter your consumer secret here
-    "username"         => "apitest",
-    "TransactionType"  => "CustomerPayBillOnline",
-    "passkey"          => "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919", //Enter your passkey here
-    "CallBackURL"      => "https://kind-terminally-sunfish.ngrok-free.app/trial/",
-    "AccountReference" => "CompanyXLTD",
-    "TransactionDesc"  => "Payment of X" ,
-);
+    ;$stmt = $db->prepare("SELECT price FROM myorders WHERE phone_number = ? AND session_id = ? ORDER BY session_id DESC LIMIT 1");
+    ;$stmt->execute([$phoneNumber, $sessionId]);
+    ;$price = $stmt->fetchColumn();
+    
+    session_start();
+
+    $errors  = array();
+    $errmsg  = '';
+
+    $config = array(
+        "env"              => "sandbox",
+        "BusinessShortCode"=> "174379",
+        "key"              => "32kCGgGrKXtdjJeinekBuNrhHrrAmW9le0KXIrPISq4Ag2HO", //Enter your consumer key here
+        "secret"           => "QYQR6e71VQSYYPO415RhMR352bap2SGea9ybUfSIT1KkgLckAGXXExvNShHc2A6t", //Enter your consumer secret here
+        "username"         => "apitest",
+        "TransactionType"  => "CustomerPayBillOnline",
+        "passkey"          => "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919", //Enter your passkey here
+        "CallBackURL"      => "https://kind-terminally-sunfish.ngrok-free.app/trial/",
+        "AccountReference" => "CompanyXLTD",
+        "TransactionDesc"  => "Payment of X" ,
+    );
 
 
-$phone = (substr($phone, 0, 1) == "+") ? str_replace("+", "", $phone) : $phone;
+    $phone = (substr($phoneNumber, 0, 1) == "+") ? str_replace("+", "", $phoneNumber) : $phoneNumber;
 
-$access_token = ($config['env']  == "live") ? "https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials" : "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"; 
+    $access_token = ($config['env']  == "live") ? "https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials" : "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"; 
     $credentials = base64_encode($config['key'] . ':' . $config['secret']); 
     
     $ch = curl_init($access_token);
@@ -33,13 +40,14 @@ $access_token = ($config['env']  == "live") ? "https://api.safaricom.co.ke/oauth
 
     $timestamp = date("YmdHis");
     $password  = base64_encode($config['BusinessShortCode'] . "" . $config['passkey'] ."". $timestamp);
-
+    
+    
     $curl_post_data = array( 
         "BusinessShortCode" => $config['BusinessShortCode'],
         "Password" => $password,
         "Timestamp" => $timestamp,
         "TransactionType" => $config['TransactionType'],
-        "Amount" => $amount,
+        "Amount" => $price,
         "PartyA" => $phone,
         "PartyB" => $config['BusinessShortCode'],
         "PhoneNumber" => $phone,
@@ -67,10 +75,4 @@ $access_token = ($config['env']  == "live") ? "https://api.safaricom.co.ke/oauth
 
     if(!preg_match('/^[0-9]{10}+$/', $phone) && array_key_exists('errorMessage', $result)){
         $errors['phone'] = $result["errorMessage"];
-    }
-
-    if($result['ResponseCode'] === "0"){         //STK Push request successful
-
-        $MerchantRequestID = $result['MerchantRequestID'];
-        $CheckoutRequestID = $result['CheckoutRequestID'];
     }
