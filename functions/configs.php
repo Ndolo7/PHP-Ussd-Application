ab1cf32463ff04cafdca552e29df2be09aac2556
Ndolo <imanindolo77@gmail.com>
2025-03-03 16:18:48 +0300

    hid configs.php

--- functions/configs.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$apiKey = "atsk_b4d03117bb56205b787b85f4ecbcae6b272f251e72ee168efabbc7766eeff53c4f22d972"; 
-
-
-$key = "vIFHfymYSiYnxtmKgNGAlEnOnedGPkSxS87wm474AxvgA8SR";
-$secret = "jQ7bJhk7VbNMKU12VCdyAqVfajGQsNjKopV2MoX9Ow6VAgCNOAAzQMgCHIZqmgxD";
-$passkey = "451acebaadb4a73d4082b50e4bc35b8e826e2741933ab67cfa614ba2c9f7488d";
